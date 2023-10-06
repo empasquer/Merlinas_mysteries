d96e002954a79267b5b0f89b0a53362c5bbbcf60
emma673r <90177139+emma673r@users.noreply.github.com>
2023-10-06 12:41:20 +0200

    locations have their own enemies and items arrays

--- src/Game.java
+++ src/Game.java
@@ -9,19 +9,35 @@ public class Game {
         // Create a list of locations
         ArrayList<Location> locations = new LocationBuilder().getLocations();
 
-        System.out.println(locations.get(0).getNorth().getName());
-        System.out.println(locations.get(3).getNorth().getName());
+/*        System.out.println(locations.get(0).getNorth().getName());
+        System.out.println(locations.get(3).getNorth().getName());*/
 
         ArrayList<Item> items = new ItemBuilder().getItems();
         ArrayList<Enemy> enemies = new EnemyBuilder().getEnemies();
 
+
         for (int i = 0; i< items.size(); i++) {
             System.out.println(items.get(i).getName());
         }
-        for (int i = 0; i< enemies.size(); i++) {
+      /*  for (int i = 0; i< enemies.size(); i++) {
             System.out.println(enemies.get(i).getName());
             System.out.println((enemies.get(i).getWeapon()).getName());
+        }*/
+
+
+      /*  System.out.println(locations.get(3).getEnemies().get(0).getName());
+
+        for (int i = 0; i< locations.get(4).getEnemies().size(); i++) {
+            System.out.println(locations.get(4).getEnemies().get(i).getName());
+        }*/
+
+                System.out.println(locations.get(2).getItems());
+
+        for (int i = 0; i< locations.get(8).getItems().size(); i++) {
+            System.out.println(locations.get(8).getItems().get(i).getName());
         }
+
+        System.out.println(locations.get(8).getItems().size());
 
 
       /*  Player merlina = new Player();
@@ -35,18 +51,44 @@ public class Game {
     private class LocationBuilder {
         public ArrayList<Location> getLocations() {
             ArrayList<Location> locations = new ArrayList<>();
+            ArrayList<Enemy> enemies = new EnemyBuilder().getEnemies();
+            ArrayList<Item> items = new ItemBuilder().getItems();
+
+
+            // Create arrays for enemies in given locations
+            ArrayList<Enemy> l3Enemies = new ArrayList<>();
+            l3Enemies.add(enemies.get(1));
+
+            ArrayList<Enemy> l4Enemies = new ArrayList<>();
+            l4Enemies.add(enemies.get(0));
+            l4Enemies.add(enemies.get(2));
+
+            // Create arrays for items on given locations
+            ArrayList<Item> l2Items = new ArrayList<>();
+            l2Items.add(items.get(7));
+
+            ArrayList<Item> l6Items = new ArrayList<>();
+            l6Items.add(items.get(0));
+
+            ArrayList<Item> l7Items = new ArrayList<>();
+            l7Items.add(items.get(4));
+
+            ArrayList<Item> l8Items = new ArrayList<>();
+            l8Items.add(items.get(1));
+            l8Items.add(items.get(3));
+
 
             // Create your locations and set their attributes here -- repeat as needed
             // ex: Location lx = new Location("name", "desc", id);
             Location l0 = new Location("Town Square", "The heart of the town where villagers gather./nIn the center of the town lies the bustling Town Square./nMerchants peddle their wares, children play games, and the townspeople go about their daily activities./nA majestic fountain graces the square's center, its waters glittering in the sunlight./nColorful banners hang from surrounding buildings, creating a festive atmosphere./nThis is the heartbeat of the town, where stories are shared, news is spread, and adventures begin.", 0);
             Location l1 = new Location("Village Inn", "A cozy inn where travelers rest and share stories./nThe Village Inn welcomes weary travelers with a warm hearth and the aroma of hearty meals./nOak beams cradle the structure, and the low ceiling gives a sense of intimacy./nAdventurers sit at wooden tables, recounting their journeys and listening to tales from distant lands./nThe innkeeper, a jovial figure, serves ale and stew to patrons, while a bard strums a lute in the corner, filling the air with a melodic tune.", 1);
-            Location l2 = new Location("Blacksmith's Forge", "The clang of metal fills the air as the blacksmith hammers away./nSparks fly as the blacksmith works tirelessly at the forge./nThe location is dominated by an anvil and various tools, their edges worn from years of use./nThe heat of the furnace radiates throughout the chamber./nThe blacksmith's hands move with precision, shaping metal into weapons, armor, and tools that will aid adventurers in their quests.", 2);
-            Location l3 = new Location("Market Street", "Stalls line the road, selling goods from near and far./nThe Market Street is a bustling thoroughfare, where traders and merchants from distant lands showcase their wares./nColorful awnings shade a variety of stalls, each offering unique items./nExotic spices, intricate textiles, and rare artifacts draw the attention of passersby./nThe air is filled with the mingling scents of spices, perfumes, and the tantalizing aroma of freshly baked bread.", 3);
-            Location l4 = new Location("Village Common", "A grassy area with benches and a clear view of the night sky./nA sense of tranquility envelops the Village Common./nSoft grass cushions the feet of those who pause to admire the night sky./nWooden benches offer a comfortable vantage point for stargazing or engaging in quiet conversation./nThe canopy of stars above seems to hold countless secrets, a testament to the mysteries of the universe that surround the town.", 4);
+            Location l2 = new Location("Blacksmith's Forge", "The clang of metal fills the air as the blacksmith hammers away./nSparks fly as the blacksmith works tirelessly at the forge./nThe location is dominated by an anvil and various tools, their edges worn from years of use./nThe heat of the furnace radiates throughout the chamber./nThe blacksmith's hands move with precision, shaping metal into weapons, armor, and tools that will aid adventurers in their quests.", 2, l2Items);
+            Location l3 = new Location("Market Street", "Stalls line the road, selling goods from near and far./nThe Market Street is a bustling thoroughfare, where traders and merchants from distant lands showcase their wares./nColorful awnings shade a variety of stalls, each offering unique items./nExotic spices, intricate textiles, and rare artifacts draw the attention of passersby./nThe air is filled with the mingling scents of spices, perfumes, and the tantalizing aroma of freshly baked bread.", 3, l3Enemies);
+            Location l4 = new Location("Village Common", "A grassy area with benches and a clear view of the night sky./nA sense of tranquility envelops the Village Common./nSoft grass cushions the feet of those who pause to admire the night sky./nWooden benches offer a comfortable vantage point for stargazing or engaging in quiet conversation./nThe canopy of stars above seems to hold countless secrets, a testament to the mysteries of the universe that surround the town.", 4, l4Enemies);
             Location l5 = new Location("Town Well", "A central well where villagers gather water and chat./nThe Town Well is a hub of activity and camaraderie./nVillagers gather here to draw fresh water, sharing tales and laughter as they take their turns at the bucket./nThe well's stone rim features intricate carvings, and flowers in colorful pots adorn its surroundings./nAt dusk, the well's magical glow creates a serene and enchanting atmosphere.", 5);
-            Location l6 = new Location("Herbalist's Hut", "Filled with the aroma of herbs, potions, and remedies./nThe air is rich with the scent of herbs in the Herbalist's Hut./nShelves lined with dried plants, jars of potions, and bundles of fragrant flowers fill the space./nThe herbalist, a wise and gentle soul, moves with practiced grace, concocting remedies to mend ailments and ailments./nVisitors are drawn to the soothing aura of the hut, seeking both cures and knowledge.", 6);
-            Location l7 = new Location("Village Chapel", "A peaceful place of worship with intricate stained glass windows./nThe Village Chapel exudes an air of reverence and tranquility./nSunlight streams through intricate stained glass windows, casting a colorful glow on the worn pews and polished wood./nVillagers gather here to reflect, pray, and seek solace./nThe chapel's altar is adorned with candles and delicate flowers, creating a peaceful sanctuary where the worries of the world can be left behind.", 7);
-            Location l8 = new Location("Mayor's Manor", "A grand house overseeing the town's affairs./nThe Mayor's Manor stands as a symbol of authority and leadership./nIvy climbs its stone walls, and a sweeping staircase leads to the entrance./nInside, opulent furnishings and portraits of ancestors grace the rooms./nThe mayor, a wise and fair individual, welcomes visitors to discuss matters of importance./nThe manor's grandeur reflects the town's history and aspirations.", 8);
+            Location l6 = new Location("Herbalist's Hut", "Filled with the aroma of herbs, potions, and remedies./nThe air is rich with the scent of herbs in the Herbalist's Hut./nShelves lined with dried plants, jars of potions, and bundles of fragrant flowers fill the space./nThe herbalist, a wise and gentle soul, moves with practiced grace, concocting remedies to mend ailments and ailments./nVisitors are drawn to the soothing aura of the hut, seeking both cures and knowledge.", 6, l6Items);
+            Location l7 = new Location("Village Chapel", "A peaceful place of worship with intricate stained glass windows./nThe Village Chapel exudes an air of reverence and tranquility./nSunlight streams through intricate stained glass windows, casting a colorful glow on the worn pews and polished wood./nVillagers gather here to reflect, pray, and seek solace./nThe chapel's altar is adorned with candles and delicate flowers, creating a peaceful sanctuary where the worries of the world can be left behind.", 7, l7Items);
+            Location l8 = new Location("Mayor's Manor", "A grand house overseeing the town's affairs./nThe Mayor's Manor stands as a symbol of authority and leadership./nIvy climbs its stone walls, and a sweeping staircase leads to the entrance./nInside, opulent furnishings and portraits of ancestors grace the rooms./nThe mayor, a wise and fair individual, welcomes visitors to discuss matters of importance./nThe manor's grandeur reflects the town's history and aspirations.", 8, l8Items);
 
             // Set direction attributes
             // l0 :
@@ -95,6 +137,14 @@ public class Game {
             l8.setEast(null);
             l8.setWest(l6);
 
+            // Add items and enemies to locations
+            l2.setItems(l2Items);
+            l3.setEnemies(l3Enemies);
+            l4.setEnemies(l4Enemies);
+            l6.setItems(l6Items);
+            l7.setItems(l7Items);
+            l8.setItems(l8Items);
+
 
             // Add locations to the list
             locations.add(l0);
@@ -109,9 +159,8 @@ public class Game {
 
             return locations;
         }
-
-
     }
+
 
     private class ItemBuilder {
 
@@ -157,9 +206,9 @@ public class Game {
         public ArrayList<Weapon> getWeapons() {
             ArrayList<Weapon> weapons = new ArrayList<>();
 
-            Weapon w0 = new Weapon("Elven Bow", "A masterfully crafted bow with arrows that seem to find their target with uncanny precision.", 85, true);
-            Weapon w1 = new Weapon("Druid's Staff", " staff carved with intricate symbols, often wielded by those who commune with nature's magic.", 89, false);
-            Weapon w2 = new Weapon("Battle Axe", "A simple yet brutal axe. Forged by dwarves.", 77, false);
+            Weapon w0 = new Weapon("Elven Bow", "A masterfully crafted bow with arrows that seem to find their target with uncanny precision.", 25, true);
+            Weapon w1 = new Weapon("Druid's Staff", " staff carved with intricate symbols, often wielded by those who commune with nature's magic.", 39, false);
+            Weapon w2 = new Weapon("Battle Axe", "A simple yet brutal axe. Forged by dwarves.", 40, false);
             Weapon w3 = new Weapon("Fangs", "A bears fangs", 34, false);
             Weapon w4 = new Weapon("Knife", "A simple knife.", 12, false);
             Weapon w5 = new Weapon("Boar Tusk", "Pointy tusks.", 20, false);
--- src/Location.java
+++ src/Location.java
@@ -1,38 +1,52 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Location {
     private String name;
     private String description;
     private int id;
-/*    private List<Item> items;
-    private List<Enemy> enemies;*/
+    private List<Item> items;
+    private List<Enemy> enemies;
     private Location north;
     private Location south;
     private Location east;
     private Location west;
 
-    public Location(String name, String description, int id) {
-        // Default constructor, no parameters
-/*        items = new ArrayList<Item>();*/
-/*        enemies = new ArrayList<Enemy>();*/
-
-
+    public Location(String name, String description, int id, Object... objects) {
         this.name = name;
         this.description = description;
         this.id = id;
+        this.items = new ArrayList<>();
+        this.enemies = new ArrayList<>();
+
+        for (Object obj : objects) {
+            if (obj instanceof Item) {
+                this.items.add((Item) obj);
+            } else if (obj instanceof Enemy) {
+                this.enemies.add((Enemy) obj);
+            }
+        }
     }
-
-    // Getters and setters for attributes
-
-    // You can add getters and setters for other attributes as needed
-
 
     public String getName() {
         return name;
     }
+
     public String getDescription() {
         return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public List<Enemy> getEnemies() {
+        return enemies;
     }
 
     public Location getNorth() {
@@ -65,5 +79,21 @@ public class Location {
 
     public void setWest(Location west) {
         this.west = west;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public void setEnemies(List<Enemy> enemies) {
+        this.enemies = enemies;
+    }
+
+    public void addItems(Item... newItems) {
+        this.items.addAll(Arrays.asList(newItems));
+    }
+
+    public void addEnemies(Enemy... newEnemies) {
+        this.enemies.addAll(Arrays.asList(newEnemies));
     }
 }
